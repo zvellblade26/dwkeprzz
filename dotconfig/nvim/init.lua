aa8da3046aa6b2ae69d0b876567ad616a18a2387
zvellblade <151661277+zvellblade26@users.noreply.github.com>
2024-11-01 08:20:46 +0700

    Add files via upload

---
+++ dotconfig/nvim/init.lua
@@ -0,0 +1,7 @@
+require("config.lazy")
+require("lazy").setup("plugins")
+
+vim.cmd.colorscheme "tokyodark"
+--vim.cmd.colorscheme "fluoromachine"
+--vim.cmd.colorscheme "solarized-osaka"
+--vim.cmd.colorscheme "gruvbox"
